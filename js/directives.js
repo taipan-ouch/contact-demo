7f04355ff32806f7daa30fbc53cb525d8ec25f58
Tim Noonan <tim.noonan@me.com>
2014-04-04 07:33:49 -0600

    added directives

--- js/directives.js
+++ js/directives.js
@@ -1,2 +1,34 @@
 /* jshint globalstrict: true*/
 "use strict";
+
+var versionFunc = function(version) {
+    return function (scope, elem, attrs) {
+        elem.text(version);
+    };
+};
+
+var integerFunc = function() {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+
+            var viewValueFunc = function(viewValue) {
+                if(/^\-?\d*$/.test(viewValue)) {
+                    ctrl.$setValidity('integer', true);
+                    return viewValue;
+                }
+                else {
+                    ctrl.$setValidity('integer', false);
+                    return undefined;
+                }
+            };
+
+            ctrl.$parsers.unshift(viewValueFunc());
+        }
+    };
+};
+
+
+var directives = angular.module('contactPath.directives', []);
+var versionDirective = directives.directive('appVersion', ['version', versionFunc]);
+versionDirective.directive('integer', integerFunc);
